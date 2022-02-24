1b97baab49282e2cdaa72f5a635331dc9395b793
Amin_Seifi <aminseiifi@gmail.com>
2022-02-24 16:54:18 +0330

    feat(password-change): Create password reset
    Create password reset templates and views, and route them.

--- account/urls.py
+++ account/urls.py
@@ -19,5 +19,17 @@ urlpatterns = [
          name='password_change'),
     path('password_change/done/',
          auth_views.PasswordChangeDoneView.as_view(),
-         name='password_change_done')
+         name='password_change_done'),
+    path('password_reset/',
+         auth_views.PasswordResetView.as_view(success_url=reverse_lazy('account:password_reset_done')),
+         name='password_reset'),
+    path('password_reset/done/',
+         auth_views.PasswordResetDoneView.as_view(),
+         name='password_reset_done'),
+    path('reset/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(),
+         name='password_reset_confirm'),
+    path('reset/done/',
+         auth_views.PasswordResetCompleteView.as_view(),
+         name='password_reset_complete')
 ]
--- pdfbaz/settings.py
+++ pdfbaz/settings.py
@@ -128,3 +128,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = 'account:dashboard'
 LOGIN_URL = 'account:login'
 LOGOUT_URL = 'account:logout'
+
+# SMTP Protocol
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
