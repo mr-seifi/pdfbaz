f7c1f4c2249370dc45a72125c682a6bc50105d9a
Amin_Seifi <aminseiifi@gmail.com>
2022-02-24 13:17:19 +0330

    feat(login): Use django Login, Logout views
    Use Django LoginView, LogoutView.

--- account/urls.py
+++ account/urls.py
@@ -1,8 +1,8 @@
+from django.contrib.auth import views as auth_views
 from django.urls import path
-
-from .views import UserLogin
 
 app_name = 'account'
 urlpatterns = [
-    path('login/', UserLogin.as_view(), name='login'),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
