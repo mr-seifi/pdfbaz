7d5ae5d489dde61fbde1e9d6aee6f90bec3be435
Amin_Seifi <aminseiifi@gmail.com>
2022-02-25 23:13:12 +0330

    feat(api): Swagger api
    Add a documentation for api by swagger.

--- api/urls.py
+++ api/urls.py
@@ -1,4 +1,6 @@
-from django.urls import path, include
+from django.urls import path, include, re_path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenRefreshView
 
@@ -9,7 +11,18 @@ app_name = 'api'
 router = DefaultRouter()
 router.register(r'book', views.BookViewSet)
 
+schema_view = get_schema_view(
+    openapi.Info(
+        title='API',
+        default_version='v1'
+    ),
+    public=True
+)
+
 urlpatterns = [
+    re_path(r'^doc(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('doc/', schema_view.with_ui('swagger', cache_timeout=0), name='swagger'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('login/', views.MyObtainTokenPairView.as_view(), name='token_obtain_pair'),
     path('login/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('register/', views.RegisterView.as_view(), name='auth_register'),
