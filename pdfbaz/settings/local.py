924b2b26b79e3c0c79e7a8eea7dfa6e2e4abc0aa
Amin_Seifi <aminseiifi@gmail.com>
2022-05-01 18:36:11 +0430

    fix(performance): Change local db
    Change local db from sqlite3 to postgresql.

--- pdfbaz/settings/local.py
+++ pdfbaz/settings/local.py
@@ -1,10 +1,11 @@
 from .base import *
-
 DEBUG = True
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'pdfbaz_db_local',
+        'USER': 'postgres',
+        'PASSWORD': 'PdF12321BAz#'
     }
 }
